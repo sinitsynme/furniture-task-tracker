a6f96d6089d101edae48048376e77c681bbffa89
sinitsynme <sinitsynme@outlook.com>
2025-01-05 17:27:46 +0400

    Initial commit

---
+++ src/main/kotlin/ru/sinitsynme/furniture_task_tracker/FurnitureTaskTrackerApplication.kt
@@ -0,0 +1,11 @@
+package ru.sinitsynme.furniture_task_tracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FurnitureTaskTrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<FurnitureTaskTrackerApplication>(*args)
+}
---
+++ src/test/kotlin/ru/sinitsynme/furniture_task_tracker/FurnitureTaskTrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.sinitsynme.furniture_task_tracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FurnitureTaskTrackerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
